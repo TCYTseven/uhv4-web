c819ba9b279c4950286766229c652795310d3ddb
Tejas Chakrapani <62619892+TCYTseven@users.noreply.github.com>
2024-12-08 17:05:44 -0500

    Update ScrollingText.tsx

--- src/components/ScrollingText.tsx
+++ src/components/ScrollingText.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 import Logo from "../assets/Logos/Hackunited3Extended.png";
 
